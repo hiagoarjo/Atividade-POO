e45621796ad6c1aca1ae421a9e2e12314c4db7d8
hiagoarjo <franciscohiago2307@gmail.com>
2026-05-22 07:47:36 -0300

    Update desafio.java

--- desafio.java
+++ desafio.java
@@ -12,5 +12,15 @@ class Carro {
         System.out.println("Marca: " + marca);
         System.out.println("Modelo: " + modelo);
         System.out.println("Ano: " + ano);
+        public void exibirInformacoes() (
+//
+//
+ESCOPO DE MÉTODO
+String mensagem = "Detalhes do carro:"; // Variável Local (morre
+// ESCOPO DE BLOCO
+//
+if (modelo |= null) {
+boolean temodelo = true; // Variável de bloco (só existe de System.out.printIn(mensagem + " Nodelo " + modelo);
+// System.out.println(temModelo); // ERRO! "temModelo" não exist
     }
 }
